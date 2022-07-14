5d2653921fa76196aad3989f07818a3daf44b0fe
Davi Marcilio <104699555+davimarcilio@users.noreply.github.com>
2022-07-14 11:42:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-function calc() {
-
-    let { adultos, crianca, duracao, resultado } = pegaelemento();
-
-
+function errors() {
+    let { adultos, crianca, duracao } = pegaelemento();
     let { duraq, qtdAdultos, qtdCrianca } = pegavalor(adultos, crianca, duracao);
 
+if(qtdAdultos <= 0){
+    adultos.value = '';
+}
+if(qtdCrianca <= 0){
+    crianca.value = '';
+}
+if(duraq <= 0){
+    duracao.value = '';
+}
+}
 
+
+function calc() {
+    let { adultos, crianca, duracao, resultado } = pegaelemento();
+
+    let { duraq, qtdAdultos, qtdCrianca } = pegavalor(adultos, crianca, duracao);
+    
     let { qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja } = calculares(duraq, qtdAdultos, qtdCrianca);
-
-
-
-       return apresenta(resultado, qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja);
+    if((qtdAdultos == '')||(duraq == '')){
+        alert('Não deixe campos vazios');
+    } else {
+        return apresenta(resultado, qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja);
  
- 
+ }
     }
 
 function pegaelemento() {
@@ -23,7 +36,7 @@ function pegaelemento() {
     return { adultos, crianca, duracao, resultado };
 }
 
-function pegavalor(adultos, crianca, duracao) {
+function pegavalor() {
     let qtdAdultos = adultos.value;
     let qtdCrianca = crianca.value;
     let duraq = duracao.value;
@@ -42,43 +55,24 @@ function apresenta(resultado, qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja) {
     resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas / 1000)}L de Bebidas</p>`;
     resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 1000)}L de Cerveja</p>`;
 }
-
- function carne(duraq) {
+function carne(duraq) {
      if (duraq >= 6){
          return 650;
      } else {
          return 400;
      }
- 
- 
- 
-    }
- 
- 
- 
- 
- 
-    function cerveja(duraq) {
+}
+function cerveja(duraq) {
     if (duraq >= 6){
         return 2000;
     } else {
         return 1200;
     }
-
-
 }
-
-
-
-
-
 function bebidas(duraq) {
     if (duraq >= 6){
         return 1500;
     } else {
         return 1000;
     }
-
-
-
 }
