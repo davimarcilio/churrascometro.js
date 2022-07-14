cbbe71039d21c0c82ce6d96c06b0500e4b50099d
Davi Marcilio <104699555+davimarcilio@users.noreply.github.com>
2022-07-13 22:50:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+function calc() {
+
+    let { adultos, crianca, duracao, resultado } = pegaelemento();
+
+
+    let { duraq, qtdAdultos, qtdCrianca } = pegavalor(adultos, crianca, duracao);
+
+
+    let { qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja } = calculares(duraq, qtdAdultos, qtdCrianca);
+
+
+
+       return apresenta(resultado, qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja);
+ 
+ 
+    }
+
+function pegaelemento() {
+    let adultos = document.getElementById('adultos');
+    let crianca = document.getElementById('crianca');
+    let duracao = document.getElementById('duracao');
+    let resultado = document.getElementById('resultado');
+    return { adultos, crianca, duracao, resultado };
+}
+
+function pegavalor(adultos, crianca, duracao) {
+    let qtdAdultos = adultos.value;
+    let qtdCrianca = crianca.value;
+    let duraq = duracao.value;
+    return { duraq, qtdAdultos, qtdCrianca };
+}
+
+function calculares(duraq, qtdAdultos, qtdCrianca) {
+    let qtdTotalCarne = carne(duraq) * qtdAdultos + (carne(duraq) / 2 * qtdCrianca);
+    let qtdTotalCerveja = cerveja(duraq) * qtdAdultos;
+    let qtdTotalBebidas = bebidas(duraq) * qtdAdultos + (bebidas(duraq) / 2 * qtdCrianca);
+    return { qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja };
+}
+
+function apresenta(resultado, qtdTotalCarne, qtdTotalBebidas, qtdTotalCerveja) {
+    resultado.innerHTML = `<p>${qtdTotalCarne / 1000}Kg de Carnes</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas / 1000)}L de Bebidas</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 1000)}L de Cerveja</p>`;
+}
+
+ function carne(duraq) {
+     if (duraq >= 6){
+         return 650;
+     } else {
+         return 400;
+     }
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+    function cerveja(duraq) {
+    if (duraq >= 6){
+        return 2000;
+    } else {
+        return 1200;
+    }
+
+
+}
+
+
+
+
+
+function bebidas(duraq) {
+    if (duraq >= 6){
+        return 1500;
+    } else {
+        return 1000;
+    }
+
+
+
+}
